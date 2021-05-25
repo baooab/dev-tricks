92fe89a3e9dc54ea53e93a7f170c37b60c801181
baooab <baooab@users.noreply.github.com>
2021-05-25 08:38:41 +0000

    Deploying to gh-pages from  @ 9fb3e8996d041e225bc90bd8a9eaf6374ad3bded 🚀

---
+++ assets/index.md.5d9cafbd.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as r,a}from"./app.d2ac5229.js";const t='{"title":"Dev Tricks","description":"","frontmatter":{},"relativePath":"index.md","lastUpdated":1621931906752}',o={},n=a('',4);o.render=function(a,t,o,l,s,i){return e(),r("div",null,[n])};export default o;export{t as __pageData};
