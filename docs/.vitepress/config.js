4fa0ecc56eaee11fb3d1466525c538454b5042a8
zhangbao <zhangbao90s@qq.com>
2021-12-27 20:17:41 +0800

    docs(typescript): update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -28,28 +28,16 @@ function getGuideSidebar() {
             text: '最近更新',
             children: [
                 {
-                    text: '每周分享第 40 期',
-                    link: '/weekly/issue-40'
-                },
-                {
-                    text: '每周分享第 39 期',
-                    link: '/weekly/issue-39'
+                    text: 'TypeScript Evolution 系列文章',
+                    link: '/typescript/README'
                 },
                 {
                     text: '将开发环境的文件换行符设置成 LF',
                     link: '/misc/eol-lf'
                 },
                 {
-                    text: '每周分享第 38 期',
-                    link: '/weekly/issue-38'
-                },
-                {
                     text: '如何使用指定了 peerDependencies 的 npm 包',
                     link: '/npm/install-peerdeps'
-                },
-                {
-                    text: '如何将 VitePress 部署到 GitHub Pages',
-                    link: '/git/vitepress-deploy-to-github'
                 }
             ]
         }
---
+++ docs/typescript/README.md
@@ -0,0 +1,6 @@
+# [TypeScript Evolution](https://mariusschulz.com/blog/series/typescript-evolution) 系列文章
+
+by Marius Schulz
+
+1. [非空类型(Non-Nullable Types)](./non-nullable-types-in-typescript.md)：TypeScript 2.0 增加了 `--strictNullChecks` flag，支持将 `null` 和 `undefined` 作为单独类型处理。
+1. [基于控制流的类型分析(Control Flow Based Type Analysis)](./control-flow-based-type-analysis-in-typescript.md)：TypeScript 2.0 新增特性。联合类型的变量在经过 `if` 判断后，类型范围会变小。
