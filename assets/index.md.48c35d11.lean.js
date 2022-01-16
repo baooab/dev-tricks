40a72156cc2d2c43de3011d8d13d4520b58770bf
baooab <zhangbao90s@qq.com>
2022-01-16 12:18:01 +0000

    Deploying to gh-pages from  @ b5a96f904f1160ec506e6a6a24e5ce0bbb160b30 🚀

--- assets/index.md.48c35d11.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as r,a}from"./app.808b26de.js";const t='{"title":"Dev Tricks","description":"","frontmatter":{},"relativePath":"index.md","lastUpdated":1642335454147}',o={},n=a('',4);o.render=function(a,t,o,l,s,i){return e(),r("div",null,[n])};export default o;export{t as __pageData};
